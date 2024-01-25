0a22102946d2dc4f2a1f93bd22ad0450f1108e5b
Igor Konoval <kip_y@sfa.org.ua>
2024-01-25 23:03:41 +0200

    edit errors

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -2,10 +2,7 @@
   Створіть тип "Gender", використовуючи union type, 
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
-type Gender = {
-  male: string;
-  female: string;
-};
+type Gender = "male" | "female";
 
 let myGender: Gender;
 
